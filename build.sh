f18fe5b3170dbaba8c8ab121b5c7f583de6bf235
ksbhandal <kamal_bhandal@hotmail.com>
2025-06-21 10:59:48 -0700

    build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-# Download and install Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt update && apt install -y ./google-chrome-stable_current_amd64.deb
+# Install Playwright dependencies
+pip install -r requirements.txt
+
+# Install browser binaries (Chromium) for Playwright
+python -m playwright install --with-deps
