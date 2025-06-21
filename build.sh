9f254d1f833cedee350580d7ca41fdf205568ede
ksbhandal <kamal_bhandal@hotmail.com>
2025-06-21 09:52:52 -0700

    build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Install Google Chrome
-apt-get update && apt-get install -y wget gnupg2
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-apt-get update && apt-get install -y google-chrome-stable
-
-# Continue with normal build
-pip install -r requirements.txt
+# Download and install Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt update && apt install -y ./google-chrome-stable_current_amd64.deb
