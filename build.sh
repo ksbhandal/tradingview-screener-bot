8f00308016ae6cc049ee993c70d9fb07f6a3dd0d
ksbhandal <kamal_bhandal@hotmail.com>
2025-06-21 09:45:41 -0700

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install Google Chrome
+apt-get update && apt-get install -y wget gnupg2
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+apt-get update && apt-get install -y google-chrome-stable
+
+# Continue with normal build
+pip install -r requirements.txt
