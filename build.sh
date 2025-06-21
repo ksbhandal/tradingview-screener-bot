bafd135dbd8d8f40b4a523460ed5701fee7f558b
ksbhandal <kamal_bhandal@hotmail.com>
2025-06-21 13:14:46 -0700

    build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 # Install Chromium browser for Python Playwright
-python -m playwright install chromium
+python -m playwright install --with-deps
