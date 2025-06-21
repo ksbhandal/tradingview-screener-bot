4bab47ef998a0c7b261ea01df73f47d3cc90d349
ksbhandal <kamal_bhandal@hotmail.com>
2025-06-21 10:15:43 -0700

    build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-# Download and install Google Chrome
+# Install Google Chrome for headless scraping
+apt-get update
+apt-get install -y wget gnupg unzip
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt update && apt install -y ./google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
+
+# Optional: Install chromedriver (if used in your script)
+# LATEST=$(wget -q -O - https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+# wget https://chromedriver.storage.googleapis.com/${LATEST}/chromedriver_linux64.zip
+# unzip chromedriver_linux64.zip
+# mv chromedriver /usr/bin/chromedriver
+# chmod +x /usr/bin/chromedriver
