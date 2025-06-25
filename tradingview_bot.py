b2c5357a0b144b5c6e165351a773aa1daaf7b74d
ksbhandal <kamal_bhandal@hotmail.com>
2025-06-24 22:45:51 -0700

    tradingview_bot.py

--- tradingview_bot.py
+++ tradingview_bot.py
@@ -50,7 +50,7 @@ def scrape_and_notify():
             "Origin": "https://www.tradingview.com",
         }
         res = requests.post(
-            "https://www.tradingview.com/markets/stocks-usa/market-movers-pre-market-gainers/",
+            "https://scanner.tradingview.com/america/scan",
             json=payload,
             headers=headers,
             timeout=10,
